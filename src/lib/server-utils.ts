567c1c1653e8c256810274b076966cc4cc698c91
abdallah ahmed <bebo73779@gmail.com>
2025-09-22 17:45:20 +0300

    changes in token prod

--- src/lib/server-utils.ts
+++ src/lib/server-utils.ts
@@ -3,9 +3,10 @@ import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
 export async function getUserToken(){
-    const encodedToken = (await cookies()).get(
-        "next-auth.session-token")
-        ?.value;
+    const cookieStore = await cookies();
+    const encodedToken =
+   cookieStore.get( "next-auth.session-token") ?.value ||
+    cookieStore.get( "__Secure-next-auth.session-token") ?.value ;
 
    const decodedToken = await decode ({
     token: encodedToken ,
